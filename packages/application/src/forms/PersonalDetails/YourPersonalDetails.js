b32d7dcf46a480fcef2315acbdb887567ddd9355
firstcreditunion <gateway@firstcu.co.nz>
2023-04-19 09:53:10 +1200

    GTM Analytics - Data Layer Test 1

--- packages/application/src/forms/PersonalDetails/YourPersonalDetails.js
+++ packages/application/src/forms/PersonalDetails/YourPersonalDetails.js
@@ -53,6 +53,10 @@ const LabelStyle = styled(Typography)(({ theme }) => ({
 }))
 
 const YourPersonalDetails = () => {
+  window.dataLayer = window.dataLayer || []
+
+  console.log('Window. Data Layer - ', (window.dataLayer = window.dataLayer || []))
+
   const defMinDobDate = new Date().setFullYear(new Date().getFullYear() - 120)
 
   const dispatch = useDispatch()
@@ -200,6 +204,7 @@ const YourPersonalDetails = () => {
   }, [isValid])
 
   function onSubmit() {
+    window.dataLayer.push({ event: 'prime_personal_details_submit' })
     console.log('Submitted')
   }
 
